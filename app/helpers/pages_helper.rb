e4ffa8443b379ffe7f91a9347fa50a25f4dbe706
Chris Aguilar <Chris@Chriss-MacBook-Pro.local>
2019-01-17 20:12:42 -0600

    Implemented RegEx parser for Tweet module to open HTML Links on a new page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,30 @@
 module PagesHelper
+  def twitter_parser tweet
+    regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+
+    tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
+
+# "henningamy: Day 9: Finished up the simple RESTful #API from yesterday, and read some more articles on APIs. Things are making m… <a href=https://t.co/tMrbTOx4w9 target='_blank'>https://t.co/tMrbTOx4w9</a>"
+
+
+# tweet = "henningamy: Day 9: Finished up the simple RESTful #API from yesterday, and read some more articles on APIs. Things are making m… https://t.co/tMrbTOx4w9"
